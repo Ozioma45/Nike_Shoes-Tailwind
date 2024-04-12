b433ea533be9027ee336aadbd7927fef04a9c545
Ozioma45 <oziomaegole@gmail.com>
2024-04-12 04:35:17 +0100

    divided the sections in the app.jsx and about to start creating the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+const App = () => (
+  <main className="relative">
+    Nav
+    <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
+    <section className="padding">popular product</section>
+    <section className="padding">super quality</section>
+    <section className="padding-x py-10">services</section>
+    <section className="padding">special offer</section>
+    <section className="padding bg-pale-blue">customer reviews</section>
+    <section className="padding-x sm:py-32 py-16 w-full">subscribe</section>
+    <section className="padding-x bg-black padding-x padding-t pb-8">
+      footer
+    </section>
+  </main>
+);
+
+export default App;
